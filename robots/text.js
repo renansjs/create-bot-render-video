d2dcbae862e97e956e7c30fbd8a2b57c018964c5
Renan Oliveira <renanoliveirasjs@gmail.com>
2020-04-02 01:57:51 -0300

    feat: add Watson natural language understanting service

--- robots/text.js
+++ robots/text.js
@@ -1,15 +1,27 @@
 const algorithmia = require('algorithmia');
-//const algorithmiaKey = require('../credentials/algorithmia.json').apyKey;
+const algorithmiaKey = require('../credentials/algorithmia.json').apikey;
 const sentenceBoundaryDetection = require('sbd');
+ const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const nlu = new NaturalLanguageUnderstandingV1({
+  authenticator: new IamAuthenticator({ apikey: watsonApiKey }),
+  version: '2018-04-05',
+  url: 'https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/ca220ce8-9305-4ec7-9be3-b6d568cc707b'
+});
 
 //criando uma interface publica.
 async function robots(content){
  await fetchContentFromWikipedia(content);
        sanitizeContent(content);
        breakContentIntoSentences(content)
+       limitMaximumSentences(content)
+ await fetchKeywordsOfAllSentences(content) 
+       
 
   async function fetchContentFromWikipedia(content) {
-    const algorithmiaAuthenticated = algorithmia('simbyY+amTigHEqm+rudZJTS3W51');
+    const algorithmiaAuthenticated = algorithmia(algorithmiaKey);
     const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2');
     const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm);
     const wikipediaContent = wikipediaResponse.get();
@@ -54,6 +66,41 @@ async function robots(content){
       })
     })
   }
+
+   //função que retorna o limite maximo de sentencas 
+   function limitMaximumSentences(content){
+    content.sentences = content.sentences.slice(0, content.maximumSentences)
+  }
+
+  //Função que preenche as keywords de cada sentença/frase
+  async function fetchKeywordsOfAllSentences(content){
+    for( const sentence of content.sentences){
+      sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+    }
+  }
+ 
+  async function fetchWatsonAndReturnKeywords(sentence) {
+    return new Promise((resolve, reject) => {
+      nlu.analyze({
+        text: sentence,
+        features: {
+          keywords: {}
+        }
+      }, (error, response) => {
+        if (error) {
+          reject(error)
+          return
+        }
+
+        const keywords = response.result.keywords.map((keyword) => {
+          return keyword.text
+        })
+
+        resolve(keywords)
+      })
+    })
+  }
+  
 
 }
 
